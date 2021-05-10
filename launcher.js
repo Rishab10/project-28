47f96a3ed3705b1c9475abf57a59a650beda0d85
Rishab10 <74807718+Rishab10@users.noreply.github.com>
2021-05-10 11:25:36 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,37 @@
+class launcher{
+constructor(body,anchor)
+{
+    var options = {bodyA:bodyB,
+        pointB:anchor,
+        stiffness:0.004,
+        lenght:1
+
+
+    }
+    this.bodyA = body
+    this.pointB = anchor
+    this.launcher = constraint.create(options)
+    world.add(world,this.launcher)
+}
+
+attach(body){
+    this.launcher.bodyA = body;
+}
+fly(){
+    this.launcher.bodyA = null;
+}
+
+display()
+{
+    if (this.launcher.bodyA)
+{
+var pointA = this.bodyA.position
+var pointB = this.pointB
+
+strokeWeight(2),
+
+line(pointA.x,pointB.x,point.y);
+
+}
+}
+}
